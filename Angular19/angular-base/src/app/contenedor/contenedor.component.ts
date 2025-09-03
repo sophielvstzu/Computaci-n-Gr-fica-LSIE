e35f943dbc99cb7a1c66c8124499a96a0ed510b0
sophielvstzu <sofiaibarraegas@gmail.com>
2025-09-03 17:56:20 -0500

    commit

--- Angular19/angular-base/src/app/contenedor/contenedor.component.ts
+++ Angular19/angular-base/src/app/contenedor/contenedor.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-contenedor',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './contenedor.component.html',
   styleUrl: './contenedor.component.css'
 })
--- Angular19/angular-base/src/app/header/header.component.ts
+++ Angular19/angular-base/src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { BuscadorComponent } from '../buscador/buscador.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LogoComponent, BuscadorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
